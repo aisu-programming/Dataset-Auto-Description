51f0b3742ff150ca1c483869aa643e6a53be6232
aisu-programming <aisu.programming@gmail.com>
2023-12-05 17:06:01 -0500

    Backup

--- main.py
+++ main.py
@@ -5,61 +5,56 @@ import subprocess
 
 class LlamaCommand():
     def __init__(self) -> None:
-        self.filename  = ''
-        self.sample    = ''
-        self.profiling = ''
+        self.filename             = ''
+        self.sample               = ''
+        self.sample_with_profiler = ''
 
     def output_filename(self, settings) -> str:
-        assert settings in list(range(6))
-
-        post_descriptions = [
-            "_only_describe",
-            "_only_describe_with_profiling",
-            "_answer_9_Qs",
-            "_answer_9_Qs_with_profiling",
-            "_answer_9_Qs_at_once",
-            "_answer_9_Qs_at_once_with_profiling",
-        ]
-        return self.filename + post_descriptions[settings]
+        assert settings in list(range(3))
+        return self.filename + f"_{settings+1}"
 
     def prompt(self, settings) -> str:
-        assert settings in list(range(6))
+        assert settings in list(range(3))
 
-        returning_prompt = \
-"""\\begin{Instruction}
-Answer the questions while using the context of the input.
-The input is a random sample of the large dataset.
-\\end{Instruction}
+        if settings == 0:
+            return \
+"""Instruction:
+Answer the questions while using the input and context.
+The input includes dataset title, hearders, a random sample, and profiler result of the large dataset.
 
-\\begin{Input}
+Input:
 """ + self.sample + """
-\\end{Input}
-"""
+Question:
+Describe the dataset in sentences.
 
-        if (settings+1) % 2 == 0:  # The odd settings are with profiling
-            returning_prompt += \
-"""
-\\begin{InputB}
-""" + self.profiling + """
-\\end{InputB}
-"""
+Answer:
+The dataset"""
 
-        if settings < 2:  # The first two settings do not contain 9 questions
-            returning_prompt += \
-""""
-\\begin{Question}
-Please describe the dataset in sentences.
-\\end{Question}
+        elif settings == 1:
+            return \
+"""Instruction:
+Answer the questions while using the input and context.
+The input includes dataset title, hearders and a random sample of the large dataset.
 
-\\begin{Answer}
-"""
+Input:
+""" + self.sample_with_profiler + """
+Question:
+Describe the dataset in sentences.
 
-        elif settings < 4:  # The 3rd and 4th settings contain 9 questions
-            returning_prompt += \
-"""
-\\begin{Question}
-Answer all the questions:
-1. Can you describe the dataset in one sentence?
+Answer:
+The dataset"""
+
+        elif settings == 2:
+            return \
+"""Instruction:
+Answer the questions while using the input and context.
+The input includes dataset title, hearders, a random sample, and profiler result of the large dataset. 
+
+Input:
+""" + self.sample_with_profiler + """
+Question:
+Considering the following nine aspects:
+1. Describe the dataset in one sentence?
 2. What does the dataset look like?
 3. Can you group the headers?
 4. What are the value types and value ranges for the most important headers?
@@ -68,35 +63,15 @@ Answer all the questions:
 7. In what format or in what way does the dataset mention location?
 8. Is there anything unclear about the data, or do you have reason to doubt the quality?
 9. Is there anything that you can point out or analyse in more detail?
-\\end{Question}
+Describe the dataset answering the nine aspects above in one compelete and coherent paragraph.
 
-\\begin{Answer}
-1. The dataset is"""
+Answer:
+The dataset"""
 
-        elif settings < 6:  # The 5th and 6th settings contain 9 questions, but ask for answering together
-            returning_prompt += \
-"""
-\\begin{Question}
-Given these 9 aspects:
-1. Can you describe the dataset in one sentence?
-2. What does the dataset look like?
-3. Can you group the headers?
-4. What are the value types and value ranges for the most important headers?
-5. Where is the data from?
-6. In what format or in what way does the dataset mention time?
-7. In what format or in what way does the dataset mention location?
-8. Is there anything unclear about the data, or do you have reason to doubt the quality?
-9. Is there anything that you can point out or analyse in more detail?
-Describe the dataset in sentences by viewing these 9 aspects.
-\\end{Question}
-
-\\begin{Answer}
-"""
-        return returning_prompt
 
     def command(self, settings) -> list:
-        assert settings in list(range(6))
-        return [ r"./llama.cpp/bin/main", "--reverse-prompt", "\\end{Answer}",
+        assert settings in list(range(3))
+        return [ r"./llama.cpp/bin/main", "--reverse-prompt", "\n",
                                           # "--interactive", "--color",
                                           "--ctx-size", "2048",
                                           "--n-predict", "-1",
@@ -104,6 +79,7 @@ Describe the dataset in sentences by viewing these 9 aspects.
                                           "--batch-size", "256",
                                           "--temp", "0.2",
                                           "--n-gpu-layers", "64",
+                                        #   "--model", "./llama.cpp/models/llama-2-7b/ggml-model-f16.gguf",
                                           "--model", "./llama.cpp/models/llama-2-13b/ggml-model-f16.gguf",
                                           # "--model", "./llama.cpp/models/CodeLlama-34b/ggml-model-q5_1.gguf",
                                           # "--repeat_penalty", "1.2",
@@ -124,30 +100,20 @@ def load_sample_txt(filename):
         return list(zip(filenames, all_sample_and_profiling))
 
 
-def split_sample_and_profiling(sample_and_profiling: str):
-    sample, profiling = sample_and_profiling.split("\n\n\n")
-    sample    = sample.replace("Dataset sample: \n", '')
-    profiling = profiling.replace("Column profiling: \n", '')
-    profiling = profiling.replace("a http://schema.org/Float", "float")
-    profiling = profiling.replace("a http://schema.org/Integer", "interger")
-    profiling = profiling[:-1]
-    return sample, profiling
-
-
 def main():
     my_command = LlamaCommand()
-    filename_sample_profiling = load_sample_txt("data/task1data/task1_sample.txt")
+    filename_sample          = load_sample_txt("data/task1data/task1_sample.txt")
+    filename_sample_profiler = load_sample_txt("data/task1data/task1_sample_profiler.txt")
 
     os.makedirs("output", exist_ok=True)
-    for filename, sample_profiling in filename_sample_profiling:
-        sample, profiling = split_sample_and_profiling(sample_profiling)
-        my_command.filename  = filename
-        my_command.sample    = sample
-        my_command.profiling = profiling
+    for (filename, sample), (_, sample_with_profiler) in zip(filename_sample, filename_sample_profiler):
+        # sample, profiling = split_sample_and_profiling(sample_profiling)
+        my_command.filename             = filename
+        my_command.sample               = sample
+        my_command.sample_with_profiler = sample_with_profiler
 
-        # for settings in range(4, 6):
-        for settings in range(6):
-            with open(f"output/{my_command.output_filename(settings)}.txt", 'w') as output_file:
+        for settings in range(3):
+            with open(f"output/log/{my_command.output_filename(settings)}.txt", 'w') as output_file:
                 subprocess.call(stdout=output_file, args=my_command.command(settings))
 
 
