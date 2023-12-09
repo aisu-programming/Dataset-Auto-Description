e8b395c6e06f004fe22448ce314060d9ebdf09ba
aisu-programming <aisu.programming@gmail.com>
2023-12-08 20:36:41 -0500

    Backup

--- main.py
+++ main.py
@@ -4,73 +4,78 @@ import subprocess
 
 
 class LlamaCommand():
-    def __init__(self) -> None:
-        self.filename             = ''
-        self.sample               = ''
-        self.sample_with_profiler = ''
+    def __init__(self, model_id, filename, sample, sample_with_profiler) -> None:
+        assert model_id in list(range(3))
+        self.model_path = [
+            "./llama.cpp/models/llama-2-7b/ggml-model-f16.gguf",
+            "./llama.cpp/models/llama-2-13b/ggml-model-f16.gguf",
+            "./llama.cpp/models/CodeLlama-34b/ggml-model-q5_1.gguf",
+        ][model_id]
+        self.filename             = filename
+        self.sample               = sample
+        self.sample_with_profiler = sample_with_profiler
 
-    def output_filename(self, settings) -> str:
-        assert settings in list(range(3))
-        return self.filename + f"_{settings+1}"
+    def output_filename(self, prompt_id) -> str:
+        assert prompt_id in list(range(3))
+        return self.filename + f"_{prompt_id+1}"
 
-    def prompt(self, settings) -> str:
-        assert settings in list(range(3))
+    def prompt(self, prompt_id) -> str:
+        assert prompt_id in list(range(3))
 
-        if settings == 0:
+        if prompt_id == 0:
             return \
 """Instruction:
 Answer the questions while using the input and context.
-The input includes dataset title, hearders, a random sample, and profiler result of the large dataset.
+The input includes dataset title, headers and a random sample of the large dataset.
 
 Input:
 """ + self.sample + """
 Question:
-Describe the dataset in sentences.
+Describe the dataset in one complete and coherent paragraph.
 
 Answer:
-The dataset"""
+The """
 
-        elif settings == 1:
+        elif prompt_id == 1:
             return \
 """Instruction:
 Answer the questions while using the input and context.
-The input includes dataset title, hearders and a random sample of the large dataset.
+The input includes dataset title, headers, a random sample, and profiler result of the large dataset.
 
 Input:
 """ + self.sample_with_profiler + """
 Question:
-Describe the dataset in sentences.
+Describe the dataset in one complete and coherent paragraph.
 
 Answer:
-The dataset"""
+The """
 
-        elif settings == 2:
+        elif prompt_id == 2:
             return \
 """Instruction:
 Answer the questions while using the input and context.
-The input includes dataset title, hearders, a random sample, and profiler result of the large dataset. 
+The input includes dataset title, headers, a random sample, and profiler result of the large dataset. 
 
 Input:
 """ + self.sample_with_profiler + """
 Question:
-Considering the following nine aspects:
-1. Describe the dataset in one sentence?
-2. What does the dataset look like?
-3. Can you group the headers?
-4. What are the value types and value ranges for the most important headers?
-5. Where is the data from?
-6. In what format or in what way does the dataset mention time?
-7. In what format or in what way does the dataset mention location?
-8. Is there anything unclear about the data, or do you have reason to doubt the quality?
-9. Is there anything that you can point out or analyse in more detail?
-Describe the dataset answering the nine aspects above in one compelete and coherent paragraph.
+Considering the following eight aspects:
+1. Summarize the dataset's overall content and purpose in one sentence.
+2. Offer an overview of its structure, including data types and general organization.
+3. Identify and group the dataset's headers, explaining their relevance and interrelationships.
+4. Detail the value types and range for key headers, emphasizing significant trends or patterns.
+5. Describe how the dataset represents time, including the format and temporal scope.
+6. Explain the representation of location in the dataset, noting specific formats or geospatial details.
+7. Discuss any ambiguities or quality concerns in the data, providing examples where necessary.
+8. Highlight any notable findings or potential areas for deeper analysis, based on your initial review.
+Describe the dataset covering the eight aspects above in one complete and coherent paragraph.
 
 Answer:
-The dataset"""
+The """
 
 
-    def command(self, settings) -> list:
-        assert settings in list(range(3))
+    def command(self, prompt_id) -> list:
+        assert prompt_id in list(range(3))
         return [ r"./llama.cpp/bin/main", "--reverse-prompt", "\n",
                                           # "--interactive", "--color",
                                           "--ctx-size", "2048",
@@ -79,13 +84,11 @@ The dataset"""
                                           "--batch-size", "256",
                                           "--temp", "0.2",
                                           "--n-gpu-layers", "64",
-                                          # "--model", "./llama.cpp/models/llama-2-7b/ggml-model-f16.gguf",
-                                          # "--model", "./llama.cpp/models/llama-2-13b/ggml-model-f16.gguf",
-                                          "--model", "./llama.cpp/models/CodeLlama-34b/ggml-model-q5_1.gguf",
+                                          "--model", self.model_path,
                                           # "--repeat_penalty", "1.2",
                                           # "--repeat-last-n", "-1",
                                           # "--no-penalize-nl",
-                                          "--prompt", self.prompt(settings) ]
+                                          "--prompt", self.prompt(prompt_id) ]
 
 
 def load_sample_txt(filename):
@@ -101,25 +104,28 @@ def load_sample_txt(filename):
 
 
 def main():
-    my_command = LlamaCommand()
-    filename_sample          = load_sample_txt("data/task1data/task1_sample.txt")
-    filename_sample_profiler = load_sample_txt("data/task1data/task1_sample_profiler.txt")
 
-    os.makedirs("output/task1_llama34b", exist_ok=True)
-    for (filename, sample), (_, sample_with_profiler) in zip(filename_sample, filename_sample_profiler):
-        # sample, profiling = split_sample_and_profiling(sample_profiling)
-        my_command.filename             = filename
-        my_command.sample               = sample
-        my_command.sample_with_profiler = sample_with_profiler
+    sample_file_list = [ "data/task1data/task1_sample", "data/task1data/task2_sample" ]
+    for sample_file in sample_file_list:
+        filename_sample          = load_sample_txt(f"{sample_file}.txt")
+        filename_sample_profiler = load_sample_txt(f"{sample_file}_profiler.txt")
 
-        for settings in range(3):
-            with open(f"output/task1_llama34b/{my_command.output_filename(settings)}.txt", 'w') as log:
-                subprocess.call(stdout=log, args=my_command.command(settings))
+        for model_id in range(3):
+            saving_dir  = "output/"
+            saving_dir += [ "task1_llama7b", "task1_llama13b", "task1_llama34b" ][model_id]
+            os.makedirs(saving_dir, exist_ok=True)
+            
+            for (filename, sample), (_, sample_with_profiler) in zip(filename_sample, filename_sample_profiler):
+                my_command = LlamaCommand(model_id, filename, sample, sample_with_profiler)
 
-            with open(f"output/task1_llama34b_p{settings+1}.txt", 'a') as description_file:
-                with open(f"output/task1_llama34b/{my_command.output_filename(settings)}.txt", 'r') as log:
-                    description = log.read().split("Answer:\n")[1][:-1]
-                    description_file.write(f'{filename}\t{description}\n')
+                for prompt_id in range(3):
+                    with open(f"{saving_dir}/{my_command.output_filename(prompt_id)}.txt", 'w') as log:
+                        subprocess.call(stdout=log, args=my_command.command(prompt_id))
+
+                    with open(f"{saving_dir}_p{prompt_id+1}.txt", 'a') as description_file:
+                        with open(f"{saving_dir}/{my_command.output_filename(prompt_id)}.txt", 'r') as log:
+                            description = log.read().split("Answer:\n")[1][:-1]
+                            description_file.write(f'{filename}\t{description}\n')
 
 
 
